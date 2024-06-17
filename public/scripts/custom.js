fec891f9ae10f40b3a59910d109e9e11a029b20e
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-06-17 12:48:09 +0100

    mobile navigation fixed

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,3 +1,5 @@
+// custom.js
+
 // Toggle dark mode function
 function toggleDarkMode() {
   const main = document.querySelector("main");
@@ -9,4 +11,30 @@ const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
 darkModeToggle.addEventListener("click", () => {
   toggleDarkMode();
+  console.log("Dark mode toggled");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuButton = document.getElementById("mobile-menu-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  mobileMenuButton.addEventListener("click", function () {
+    console.log("Hamburger button clicked");
+
+    // Toggle visibility of mobile menu
+    if (
+      mobileMenu.style.display === "none" ||
+      mobileMenu.style.display === ""
+    ) {
+      mobileMenu.style.display = "flex";
+      console.log("Mobile menu opened");
+    } else {
+      mobileMenu.style.display = "none";
+      console.log("Mobile menu closed");
+    }
+
+    // Set aria-expanded attribute for accessibility
+    const isVisible = mobileMenu.style.display === "flex";
+    mobileMenuButton.setAttribute("aria-expanded", isVisible);
+  });
 });
